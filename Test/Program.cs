d1a9e3a0b99c0fb75f26382973403db9f44ff1a5
AndySunSS <1679981610@qq.com>
2018-04-09 19:26:23 +0800

    添加项目文件。

---
+++ Test/Program.cs
@@ -0,0 +1,24 @@
+﻿//using System;
+//using System.Xml;
+using ClassLibrary1;
+
+namespace Test
+{
+    public class Program
+    {
+        private int _secret;
+
+        public int Secret
+        {
+            get { return --_secret; }
+            set { _secret = value; }
+        }
+
+
+        static void Main(string[] args)
+        {
+        }
+
+        
+    }
+}
